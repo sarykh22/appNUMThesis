815d526bf33bae4bdb8105d53ff4e8700c7b7cda
sary <saryr.r22@gmail.com>
2024-10-26 15:44:00 +0700

    add file

--- app/src/main/java/com/example/appnumthesis/FavoriteFragment.kt
+++ app/src/main/java/com/example/appnumthesis/FavoriteFragment.kt
@@ -1,11 +1,10 @@
-package com.example.navigationtesting
+package com.example.appnumthesis
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.appnumthesis.R
+import androidx.fragment.app.Fragment
 
 class FavoriteFragment : Fragment() {
 
--- app/src/main/java/com/example/appnumthesis/HomeFragment.kt
+++ app/src/main/java/com/example/appnumthesis/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.example.navigationtesting
+package com.example.appnumthesis
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.appnumthesis.R.layout
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +13,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(layout.fragment_home, container, false)
     }
 
 }
